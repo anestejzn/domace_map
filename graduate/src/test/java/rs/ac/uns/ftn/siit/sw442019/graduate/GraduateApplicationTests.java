0f3216abb6fde0c649eee3b1af240247f0097ed3
anestejzn <anastasijas12@gmail.com>
2023-09-12 19:41:52 +0200

    project

---
+++ graduate/src/test/java/rs/ac/uns/ftn/siit/sw442019/graduate/GraduateApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.siit.sw442019.graduate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
